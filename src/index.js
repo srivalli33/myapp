7ff5f78e0715c917edffe441ef09dd1d4f695294
srivalli <srivalli@lyrostech.com>
2024-03-11 15:24:11 +0530

    child comit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import './post.js'
-import './feeds.js'
-import feeds from './feeds';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import './post.js'
+// import './feeds.js'
+// import feeds from './feeds';
+// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <feeds />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App13a from "./App13a";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <feeds />
-  </React.StrictMode>
+  <>
+    <App13a />
+  </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
