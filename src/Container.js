d21ca4bb1f5e0626f37c46c588b5a7b7acfdf8a2
srivalli <srivalli@lyrostech.com>
2024-03-20 18:22:10 +0530

    commit

--- src/Container.js
+++ src/Container.js
@@ -3,5 +3,5 @@ import Feeds from "./Feeds"
 import Post from "./Post"
 export default function Container(props) {
   if (props.flag === 1) return <Feeds />;
-  else if (props.flag === 2) return <Post />;
+  else if  (props.flag === 2) return <Post />;
 }    
